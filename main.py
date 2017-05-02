66bf17bad6da78028f4849235847329ebf754660
Roberng <ROBERTO.carlos.navarrete@gmail.com>
2017-05-02 23:01:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,28 @@ def cambiarPrograma(numeroDeProgramas):
         pyautogui.keyUp('tab')
         vecesParaCambiar = vecesParaCambiar+1
     pyautogui.keyUp('alt')
+    time.sleep(0.6) #He puesto esta linea para que espere 6 segundos después
+                    #de cambiar de programa, puede eliminarse o ajustarse en diferentes
+                    #configuraciones"""
 
+#En word, para cambiar cada celda de la tabla en la que se encuentran los datos
 
+def cambiarCeldaWord(numeroDeCeldas):
+    pyautogui.PAUSE=0.3
+    for i in range(numeroDeCeldas):
+        pyautogui.keyDown('tab')
 
+#para copiar el texto
+
+def copiar():
+    pyautogui.hotkey('ctrl', 'c')
+
+#para pegar el texto
+
+def pegar():
+    pyautogui.hotkey('ctrl', 'v')
+
+#cambiarPrograma(2)
+#copiar()
+#cambiarCeldaWord(6)
+#pegar()
