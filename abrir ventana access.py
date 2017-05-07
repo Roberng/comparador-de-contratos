faf0bee02871a3a19ef8b53eaf28634b43c04b2d
Roberng <ROBERTO.carlos.navarrete@gmail.com>
2017-05-07 22:17:49 +0200

    Delete abrir ventana access.py

--- abrir ventana access.py
+++
@@ -1,7 +0,0 @@
-import pywinauto
-
-pwa_app = pywinauto.application.Application()
-w_handle = pywinauto.findwindows.find_windows(title=u'Settings', class_name='ApplicationFrameWindow')[0]
-window = pwa_app.window_(handle=w_handle)
-window.SetFocus()
-
